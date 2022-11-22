91812a8754b7eca4e3fe332f9ffc5d4f0beb5b88
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2022-11-22 17:23:54 +0100

    works

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ fn main() -> xr::Result<()> {
     let available_extensions = xr.enumerate_extensions()?;
     assert!(available_extensions.khr_d3d12_enable);
     assert!(available_extensions.msft_holographic_remoting);
+    fn default<T:Default>() -> T { T::default() }
     let mut enabled_extensions = xr::ExtensionSet::default();
     enabled_extensions.khr_d3d12_enable = true;
     enabled_extensions.msft_holographic_remoting = true;
-    let xr = xr.create_instance(&xr::ApplicationInfo{application_name: "IR", engine_name: "Rust", ..Default::default()}, &enabled_extensions, &[])?;
+    let xr = xr.create_instance(&xr::ApplicationInfo{application_name: "IR", engine_name: "Rust", ..default()}, &enabled_extensions, &[])?;
     let system = xr.system(xr::FormFactor::HEAD_MOUNTED_DISPLAY)?;
     xr.graphics_requirements::<xr::D3D12>(system)?; // Microsoft Holographic Remoting implementation fails to create session without this call    
     xr::cvt(unsafe{(xr.exts().msft_holographic_remoting.unwrap().remoting_connect)(xr.as_raw(), system, &xr::sys::RemotingConnectInfoMSFT {
@@ -19,8 +20,8 @@ fn main() -> xr::Result<()> {
             secure_connection: false.into(),
     })}).unwrap();
     use pollster::FutureExt as _;
-    let adapter = wgpu::Instance::new(wgpu::Backend::Dx12.into()).request_adapter(&Default::default()).block_on().unwrap();
-    let (device, queue) = adapter.request_device(&Default::default(), None).block_on().unwrap();
+    let adapter = wgpu::Instance::new(wgpu::Backend::Dx12.into()).request_adapter(&default()).block_on().unwrap();
+    let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor{features: wgpu::Features::MULTIVIEW, ..default()}, None).block_on().unwrap();
     use wgpu_hal::api::Dx12;
     let (session, mut frame_wait, mut frame_stream) = unsafe {
         let (device, queue) = device.as_hal::<Dx12, _, _>(|device| (device.unwrap().raw_device().as_mut_ptr(), device.unwrap().raw_queue().as_mut_ptr()));
@@ -37,10 +38,10 @@ fn main() -> xr::Result<()> {
         layout: Some(&pipeline_layout),
         vertex: wgpu::VertexState{module: &vert_shader, entry_point: "fullscreen_vertex_shader", buffers: &[]},
         fragment: Some(wgpu::FragmentState{module: &frag_shader, entry_point: "debug_pattern_fragment_shader", targets: &[Some(format.into())]}),
-        primitive: wgpu::PrimitiveState::default(),
+        primitive: default(),
         depth_stencil: None,
-        multisample: wgpu::MultisampleState::default(),
-        multiview: (views.len() > 1).then(|| (views.len() as u32).try_into().ok().unwrap()),
+        multisample: default(),
+        multiview: None//(views.len() > 1).then(|| (views.len() as u32).try_into().ok().unwrap()),
     });
     if views.len() == 2 { assert_eq!(views[0], views[1]); } else { assert!(views.len()==1); }
     let xr::ViewConfigurationView{recommended_image_rect_width: width, recommended_image_rect_height: height, ..} = views[0];
@@ -51,51 +52,46 @@ fn main() -> xr::Result<()> {
         sample_count: 1,
         width, height,
         face_count: 1,
-        array_size: views.len() as u32,
+        array_size: 1,//views.len() as u32,
         mip_count: 1,
     })?;
     let images = swapchain.enumerate_images()?.into_iter().map(|image| {
-        let desc = wgpu::TextureDescriptor {label: None, size: wgpu::Extent3d{width, height, depth_or_array_layers: views.len() as u32}, mip_level_count: 1, sample_count: 1, dimension: wgpu::TextureDimension::D2, format, usage: wgpu::TextureUsages::RENDER_ATTACHMENT|wgpu::TextureUsages::TEXTURE_BINDING};
+        let desc = wgpu::TextureDescriptor {label: None, size: wgpu::Extent3d{width, height, depth_or_array_layers: 1/*views.len() as u32*/}, mip_level_count: 1, sample_count: 1, dimension: wgpu::TextureDimension::D2, format, usage: wgpu::TextureUsages::RENDER_ATTACHMENT|wgpu::TextureUsages::TEXTURE_BINDING};
         unsafe{device.create_texture_from_hal::<Dx12>(<Dx12 as wgpu_hal::Api>::Device::texture_from_raw(d3d12::Resource::from_raw(image.cast()), desc.format, desc.dimension, desc.size, desc.mip_level_count, desc.sample_count), &desc)}
     }).collect::<Box<_>>();
-    let stage = session.create_reference_space(xr::ReferenceSpaceType::STAGE, xr::Posef::IDENTITY)?;
+    let reference_space = session.create_reference_space(xr::ReferenceSpaceType::VIEW, xr::Posef::IDENTITY)?;
     loop {
-        let mut session_running = false;
         let mut event_storage = xr::EventDataBuffer::new();
         while let Some(event) = xr.poll_event(&mut event_storage)? {
             use xr::Event::*; match event {
-                SessionStateChanged(e) => {
-                    println!("entered state {:?}", e.state());
-                    match e.state() {
-                        xr::SessionState::READY => { session.begin(view_type)?; session_running = true; }
-                        xr::SessionState::STOPPING => { session.end()?; session_running = false; }
-                        xr::SessionState::EXITING | xr::SessionState::LOSS_PENDING => { return Ok(()); }
-                        _ => {dbg!()}
-                    }
-                }
+                SessionStateChanged(e) => {use xr::SessionState::*; match e.state() {
+                    IDLE|SYNCHRONIZED|VISIBLE|FOCUSED => {},
+                    READY => { session.begin(view_type)?; println!("Ready"); }
+                    STOPPING => { session.end()?; println!("Stopping"); return Ok(()); }
+                    EXITING|LOSS_PENDING => { println!("Exiting|LossPending"); return Ok(()); }
+                    _ => panic!("{:?}", e.state())
+                }}
                 InstanceLossPending(_) => { return Ok(()); }
-                EventsLost(e) => { println!("lost {} events", e.lost_event_count()); }
                 _ => {dbg!()}
             }
         }
-        assert!(session_running);
         let frame_state = frame_wait.wait()?;
         frame_stream.begin()?;
         let environment_blend_mode = xr::EnvironmentBlendMode::ADDITIVE;
         if !frame_state.should_render { dbg!(); frame_stream.end(frame_state.predicted_display_time, environment_blend_mode, &[])?; continue; }
         let index = swapchain.acquire_image()? as usize;
         swapchain.wait_image(xr::Duration::INFINITE)?;
-        let ref view = images[index].create_view(&wgpu::TextureViewDescriptor{base_array_layer: 0, array_layer_count: (views.len() as u32).try_into().ok(), ..Default::default()});
+        let ref view = images[index].create_view(&wgpu::TextureViewDescriptor{base_array_layer: 0, array_layer_count: 1/*(views.len() as u32)*/.try_into().ok(), ..default()});
         let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
         {let mut pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor{label: None,
             color_attachments: &[Some(wgpu::RenderPassColorAttachment{view, resolve_target: None, ops: wgpu::Operations{load: wgpu::LoadOp::Clear(wgpu::Color::GREEN), store: true}})], depth_stencil_attachment: None});
             pass.set_pipeline(&render_pipeline);
             pass.draw(0..3, 0..1);}
-        let (_, views) = session.locate_views(view_type, frame_state.predicted_display_time, &stage)?;
+        let (_, views) = session.locate_views(view_type, frame_state.predicted_display_time, &reference_space)?;
         queue.submit(Some(encoder.finish()));
         swapchain.release_image()?;
         let rect = xr::Rect2Di {offset: xr::Offset2Di{x: 0, y: 0}, extent: xr::Extent2Di{width: width as i32, height: height as i32}};
-        frame_stream.end(frame_state.predicted_display_time, environment_blend_mode, &[&xr::CompositionLayerProjection::new().space(&stage).views(&[0,1].map(|i|
-            xr::CompositionLayerProjectionView::new().pose(views[i].pose).fov(views[i].fov).sub_image(xr::SwapchainSubImage::new().swapchain(&swapchain).image_array_index(i as u32).image_rect(rect))))])?;
+        frame_stream.end(frame_state.predicted_display_time, environment_blend_mode, &[&xr::CompositionLayerProjection::new().space(&reference_space).views(&[0,1].map(|i|
+            xr::CompositionLayerProjectionView::new().pose(views[i].pose).fov(views[i].fov).sub_image(xr::SwapchainSubImage::new().swapchain(&swapchain).image_array_index(/*i as u32*/0).image_rect(rect))))])?;
     }
 }
